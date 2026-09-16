af86f981c2f9ce7dd1894157f3906bdf717975f2
eya568 <swayedaya@gmail.com>
2026-09-16 16:45:48 +0100

    testing file and edited requirements

---
+++ app/test_app.py
@@ -0,0 +1,10 @@
+from app import app
+
+
+def test_index():
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json["message"] == "Source To Cloud - Capstone API"
